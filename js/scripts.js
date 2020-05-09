3649b7bfc1f71350e37eaadf3910711e15a91c2d
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-05-09 19:15:14 +0300

    Add javascript function that checks whether name and email is entered

--- js/scripts.js
+++ js/scripts.js
@@ -5,33 +5,3 @@ var message = function (name, email) {
     alert(name + "We have received your message.Thankyou for reaching out to us");
   }
 }
-
-
-$(document).ready(function(){
-  $(".details-section1").click(function(){
-    $(".icon1").toggle();
-    $(".details1").toggle();
-  });
-  $(".details-section2").click(function(){
-    $(".icon2").toggle();
-    $(".details2").toggle();
-  });
-  $(".details-section3").click(function(){
-    $(".icon3").toggle();
-    $(".details3").toggle();
-  });
-});
-$(document).ready(function(){
-  $("hover-image").hover(function(){
-    $(".text").fadeToggle();
-  });
-});
-$(document).ready(function(){
-  $(".form").submit(function(event){
-    event.preventDefault();
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var comment = $("#message").val();
-    message(name,email);
-  });
-});
