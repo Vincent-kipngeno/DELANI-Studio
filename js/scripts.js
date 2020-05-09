9fece55f31f514deba6c5a9bdc75c78744e730f9
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-05-09 19:18:06 +0300

    Add javascript function that enables toggling to what we do section

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,19 @@ var message = function (name, email) {
     alert(name + "We have received your message.Thankyou for reaching out to us");
   }
 }
+
+
+$(document).ready(function(){
+  $(".details-section1").click(function(){
+    $(".icon1").toggle();
+    $(".details1").toggle();
+  });
+  $(".details-section2").click(function(){
+    $(".icon2").toggle();
+    $(".details2").toggle();
+  });
+  $(".details-section3").click(function(){
+    $(".icon3").toggle();
+    $(".details3").toggle();
+  });
+});
