f519b3173f03123f4dff035f1c2527b8a0207e6d
Vincent Kipngeno <kipngenovi2001@gmail.com>
2020-05-09 18:11:46 +0300

    Change directory c, s and h_image, Add bootstrap.css and Add header to index.html and links to css, javascript, jquery and bootstrap.

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+var message = function (name, email) {
+  if (!name || !email) {
+    alert("Please enter your name and email");
+  } else {
+    alert(name + "We have received your message.Thankyou for reaching out to us");
+  }
+}
+
+
+$(document).ready(function(){
+  $(".details-section1").click(function(){
+    $(".icon1").toggle();
+    $(".details1").toggle();
+  });
+  $(".details-section2").click(function(){
+    $(".icon2").toggle();
+    $(".details2").toggle();
+  });
+  $(".details-section3").click(function(){
+    $(".icon3").toggle();
+    $(".details3").toggle();
+  });
+});
+$(document).ready(function(){
+  $("hover-image").hover(function(){
+    $(".text").fadeToggle();
+  });
+});
+$(document).ready(function(){
+  $(".form").submit(function(event){
+    event.preventDefault();
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var comment = $("#message").val();
+    message(name,email);
+  });
+});
